c5fb010046697234292a52a0507aca665da11b2b
jakwalma <jakob.walmann@outlook.com>
2022-11-17 08:16:16 +0100

    Using the correct api url for local or deployed

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,9 @@
+function getURL() {
+    return process.env.NODE_ENV === "development" ? "http://localhost:7071/api/" : "https://celestar-api.azurewebsites.net/api/"
+}
+
 export async function getAllCreatures() {
-    return fetch("http://localhost:7071/api/GetCreatures")
+    return fetch(getURL() + "AllCreatures")
         .then(response => {
             if (response.status !== 200) {
                 throw new Error('Couldn\'t fetch monsters!')
